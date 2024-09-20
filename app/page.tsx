75c288df84522057b61dd43c60eb28a89f03a5d6
waxit-dev <info@waxit.com.au>
2024-09-20 18:08:24 +1000

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,90 @@
-import Image from "next/image";
-
+import { Forklift, Monitor, UserRound } from "lucide-react";
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    return (
+        <div className="grid grid-rows-[20px_1fr_20px] min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <main className="flex flex-col row-start-2 items-center sm:items-start">
+                <div className="flex flex-col h-5 mb-4">
+                    <h1>Project Quadrigis Electrix</h1>
+                </div>
+                <div className="grid w-full h-full grid-cols-[30%_70%]">
+                    <div className="grid border border-gray-300 grid-rows-2">
+                        <div className="grid justify-center items-center">
+                            <Monitor size={240} strokeWidth={0.5} />
+                        </div>
+                        <div className="grid border-t border-gray-300 justify-center items-center">
+                            <Forklift size={240} strokeWidth={0.5} />
+                        </div>
+                    </div>
+                    <div className="grid gap-0 border border-l-0 border-gray-300">
+                        {/* Top row with 4 columns */}
+                        <div className="grid grid-cols-3 divide-x divide-gray-300">
+                            <div className="p-6 flex flex-col justify-evenly items-center">
+                                <p>CS</p>
+                                <UserRound size={120} strokeWidth={0.25} className="border border-gray-300 rounded-full" />
+                                <div className="flex flex-col">
+                                    <p>Overall: 99</p>
+                                    <p>Punctuality: 88</p>
+                                    <p>Social: 77</p>
+                                    <p>Productivity: 66</p>
+                                    <p>Intensity: 55</p>
+                                    <p>Specialty: 44</p>
+                                </div>
+                            </div>
+                            <div className="p-6 flex flex-col justify-evenly items-center">
+                                <p>CC</p>
+                                <UserRound size={120} strokeWidth={0.25} className="border border-gray-300 rounded-full" />
+                                <div className="flex flex-col">
+                                    <p>Overall: 99</p>
+                                    <p>Punctuality: 88</p>
+                                    <p>Social: 77</p>
+                                    <p>Productivity: 66</p>
+                                    <p>Intensity: 55</p>
+                                    <p>Specialty: 44</p>
+                                </div>
+                            </div>
+                            <div className="p-6 flex flex-col justify-evenly items-center">
+                                <p>PROD</p>
+                                <UserRound size={120} strokeWidth={0.25} className="border border-gray-300 rounded-full" />
+                                <div className="flex flex-col">
+                                    <p>Overall: 99</p>
+                                    <p>Punctuality: 88</p>
+                                    <p>Social: 77</p>
+                                    <p>Productivity: 66</p>
+                                    <p>Intensity: 55</p>
+                                    <p>Specialty: 44</p>
+                                </div>
+                            </div>
+                        </div>
+                        {/* Bottom row with 3 columns */}
+                        <div className="grid grid-cols-2 divide-x divide-gray-300 border-t border-gray-300">
+                            <div className="p-6 flex flex-col justify-evenly items-center">
+                                <p>PI</p>
+                                <UserRound size={120} strokeWidth={0.25} className="border border-gray-300 rounded-full" />
+                                <div className="flex flex-col">
+                                    <p>Overall: 99</p>
+                                    <p>Punctuality: 88</p>
+                                    <p>Social: 77</p>
+                                    <p>Productivity: 66</p>
+                                    <p>Intensity: 55</p>
+                                    <p>Specialty: 44</p>
+                                </div>
+                            </div>
+                            <div className="p-6 flex flex-col justify-evenly items-center">
+                                <p>PA</p>
+                                <UserRound size={120} strokeWidth={0.25} className="border border-gray-300 rounded-full" />
+                                <div className="flex flex-col">
+                                    <p>Overall: 99</p>
+                                    <p>Punctuality: 88</p>
+                                    <p>Social: 77</p>
+                                    <p>Productivity: 66</p>
+                                    <p>Intensity: 55</p>
+                                    <p>Specialty: 44</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
